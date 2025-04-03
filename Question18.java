c7b59871d7eb7b869bb69ef6321e9b5f2b2e7511
Krishna3881 <118133836+Krishna3881@users.noreply.github.com>
2025-04-03 06:01:23 +0530

    Add files via upload

---
+++ Question18.java
@@ -0,0 +1,32 @@
+//Given 2 integers a & b.Swap the 2 given values using
+//sum and difference method
+// OUTPUT
+// Enter the 1 element:
+// 5
+// Enter the 2 element:
+// 6
+// Values before Swap:
+// a: 5 b: 6
+// Values after Swap:
+// a: 6 b: 5
+import java.util.Scanner;
+public class Question18{
+    static Scanner sc=new Scanner(System.in);
+    static void swap(int a,int b){
+        System.out.println("Values before Swap: ");
+        System.out.println("a: "+a+" b: "+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("Values after Swap: ");
+        System.out.println("a: "+a+" b: "+b);
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the 1 element: ");
+        int a=sc.nextInt();
+        System.out.println("Enter the 2 element: ");
+        int b=sc.nextInt();
+        swap(a, b);
+        sc.close();
+    }
+}
